ae19f11f04f88f2decb80c700b140a58b968574d
thinhdanggroup <dinhluchvc@gmail.com>
2023-09-02 15:22:19 +0700

    remove comment

--- cython_example/util.py
+++ cython_example/util.py
@@ -11,12 +11,6 @@ def extract_blog_posts(content):
     blog_posts = soup.find_all('article', class_='archive__item')
 
     for post in blog_posts:
-        """
-        <h2 class="archive__item-title no_toc" itemprop="headline">
-<a href="/function-calling-openai/" rel="permalink">Demystifying Function Calling in OpenAI: An In-Depth Guide
-</a>
-</h2>
-        """
         title = post.find('h2', class_='archive__item-title').text.strip()
         link = post.find('a')['href']
         print(f'Title: {title}\nLink: {link}\n')
