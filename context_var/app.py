d13897f090006bb3b91627a8dd54e622a393c32b
thinhdanggroup <dinhluchvc@gmail.com>
2024-05-06 17:02:44 +0700

    update example

--- context_var/app.py
+++ context_var/app.py
@@ -1,19 +1,22 @@
 from contextvars import ContextVar
 
-## Create a new ContextVar with a name and optional default value
-var: ContextVar[int] = ContextVar('var', default=42)
+# Create a new ContextVar with a name and optional default value
+var: ContextVar[int] = ContextVar('var')
 
-## Set a new value for the context variable in the current context
-token = var.set('new value')
-token = var.set('new value 1')
+# Set a new value for the context variable in the current context
+token1 = var.set('new value')
+token2 = var.set('new value 1')
 
-## Get the value for the context variable in the current context
+# Get the value for the context variable in the current context
+print(var.get())  # Outputs: new value 1
+
+# Reset the context variable to its previous value
+var.reset(token2)
+
 print(var.get())  # Outputs: new value
+var.reset(token1)
 
-## Reset the context variable to its previous value
-var.reset(token)
-
-## Now the variable has no value again, so var.get() would raise a LookupError
+# Now the variable has no value again, so var.get() would raise a LookupError
 try:
     print(var.get())
 except LookupError:
